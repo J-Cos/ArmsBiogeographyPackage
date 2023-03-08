dd2c0d10bcd118ac7dbef28a04f46dd9eb650ca4
J <j.williams20@imperial.ac.uk>
2023-03-08 15:58:56 +0000

    Added stress to biogeographic variables added
    Stress from Andrello et al., 2022

--- R/GetBiogeographicVariablesForARMS().R
+++ R/GetBiogeographicVariablesForARMS().R
@@ -1,23 +1,121 @@
 #' Get biographic variables
 #'
 #' This function accepts a dataframe with columns of 'ARMS' 'Site_Longitude' and 'Site_Latitude'
-#' and returns a dataframe with the seamount area and isolation associated with each ARMS unit. 
+#' and returns a dataframe with the seamount area, isolation and stressor values associated with each ARMS unit. 
 #'
-#' @param dat inout dataframe
-#' @param shallowOceanBuffer the radius of the circle within which to calculate the proportion of shallow ocean (metres). default 50km
-#' @param shallowDepth the depth of ocean up to which it is considered shallow, default 200m
+#' @param dat input dataframe with three columns titles: ARMS, Site_Longitude, Site_Latitude
+#' @param shallowOceanBuffer the radius of the circle within which to calculate the proportion of shallow ocean (metres). Default 50km
+#' @param shallowDepth the depth of ocean up to which it is considered shallow, Default 200m
+#' @param stressRadius the radius of the circle from which the nearest stress value is drawn, default (metres). Default 30km
 #' @return A dataframe containing the ARMs identifier, area, and distance. Also contains the LatLong of the closest point on the continental shelf and the id of that continental shelf (eurasian (1) or australasian (2))
 #' @export
 
-GetBiogeographicVariablesForARMS<- function(dat, shallowOceanBuffer=50000, shallowDepth=200) {
+GetBiogeographicVariablesForARMS<- function(dat, shallowOceanBuffer=50000, shallowDepth=200, stressRadius=30000) {
 
     # 1. Packages and path to project directory ######
         require(geosphere)
         require(igraph)
         require(raster)
+        require(rgdal)
+        require(rgeos)
+        require(sf)
         require(sp)
         require(tidyverse)
 
+    #functions
+    extract_values <- function(data, allreefs, max.radius) {
+
+        # If allreefs is sf, convert it to SpatialPolygonsDataFrame
+        if (is(allreefs, "sf")) {
+            cat("Converting allreefs to SpatialPolygonsDataFrame...")
+            allreefs <- sf::as_Spatial(allreefs)
+            cat("done\n")
+        }
+        if (!is(allreefs, "SpatialPolygonsDataFrame")) stop("allreefs must be of class sf or SpatialPolygonsDataFrame")
+
+        # Read CRS for allreefs
+        prj4 <- sp::proj4string(allreefs)
+
+        cat("CRS for allreefs is\n", prj4, "\n")
+
+        # Convert points to spatialPoints. Assuming input data are in WGS84 (EPSG: 4326)
+        points <- sp::SpatialPoints(data[, c(1, 2)],
+            proj4string = sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
+        )
+
+
+        # Change CRS for points
+        points_prj4 <- sp::spTransform(points, sp::CRS(prj4))
+
+        # Extract allreefs centroids
+        allreefs_centroids <- rgeos::gCentroid(allreefs, byid = T)
+
+        # Find nearest neighboring allreefs for each point
+        nn <- RANN::nn2(sp::coordinates(allreefs_centroids),
+            sp::coordinates(points_prj4),
+            k = 1,
+            searchtype = "radius",
+            radius = max.radius
+        )
+
+
+        # Initializing output dataframe
+        # LEGEND:
+        # "score", Climate: composite score
+        # "scorecn", Climate: connectivity
+        # "scorecy", Climate: Cyclone Risk
+        # "scorepfc", Climate: Thermal future
+        # "scoreth", Climate: Thermal history
+        # "scoretr", Climate: Recent stress
+        # "grav_NC", Fishing: Market Pressure
+        # "sediment", Pollution: Sedimentation
+        # "nutrient", Pollution: Nutrients
+        # "pop_count", Coastal Development: Human Population
+        # "num_ports", Industrial Development: Ports
+        # "reef_value", Tourism: Reef Value
+        out.data <- matrix(NA, nrow = nrow(nn$nn.idx), ncol = 13)
+        colnames(out.data) <- c(
+            "score",
+            "scorecn",
+            "scorecy",
+            "scorepfc",
+            "scoreth",
+            "scoretr",
+            "grav_NC",
+            "sediment",
+            "nutrient",
+            "pop_count",
+            "num_ports",
+            "reef_value",
+            "cumul_score"
+        )
+        out.data <- as.data.frame(out.data)
+
+        # Loop on points to fill values into output dataframe
+        for (i in 1:nrow(nn$nn.idx)) {
+            if (nn$nn.idx[i, 1] == 0) next
+            out.data[i, ] <- as.list(allreefs@data[
+            nn$nn.idx[i, 1],
+            c(
+                "score",
+                "scorecn",
+                "scorecy",
+                "scorepfc",
+                "scoreth",
+                "scoretr",
+                "grav_NC",
+                "sediment",
+                "nutrient",
+                "pop_count",
+                "num_ports",
+                "reef_value",
+                "cumul_score"
+            )
+            ])
+        }
+        out.data <- cbind(data, out.data)
+        return(out.data)
+    }
 
     #hardcoded parameters
         NumberShelves=2 #this is hardcoded as there is no other sensible number given 
@@ -40,6 +138,9 @@ GetBiogeographicVariablesForARMS<- function(dat, shallowOceanBuffer=50000, shall
             load(file="data/etopo.rda")
             print("Bathymetry data loaded")
 
+        # Load stress data
+            load('/home/j/Dropbox/CrossPacific_Paper/Data/allreefs.RData')
+            print("Stress data loaded")
 
     #areas
     print("Calculating shallow sea areas...")
@@ -82,13 +183,22 @@ GetBiogeographicVariablesForARMS<- function(dat, shallowOceanBuffer=50000, shall
 
         #get distance
             distances<-geosphere::dist2Line(p, shelfPolys)
-            distances[,1]<-distances[,1]/1000
+            distances[,1]<-round(distances[,1]/1000, digits=-1)
             print("Isolation calculation complete")
+
+    #stress
+        print("Extracting stress...")
+        footprint<-extract_values(data=d, allreefs, max.radius = stressRadius) %>%
+            dplyr::select(-c(Site_Longitude, Site_Latitude))
+        print("Stress extraction complete")
+
 
     #MERGE THIS INTO DATA
         new_dat<-proportionshallow_df %>% 
-            cbind(., distances)
-        colnames(new_dat)<-c("ARMS", "proportionshallow", "distance(km)", "NearestShelf_Longitude", "NearestShelf_Latitude", "ShelfMatch")
+            cbind(., distances) %>%
+            cbind(., footprint)
+        colnames(new_dat)[1:6]<-c("ARMS", "proportionshallow", "distance(km)", "NearestShelf_Longitude", "NearestShelf_Latitude", "ShelfMatch")
+        
     
     return(new_dat)
 }
