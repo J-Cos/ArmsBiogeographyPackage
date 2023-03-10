d68f035146de5b366f6e176624fb08ca7da2f6eb
J <j.williams20@imperial.ac.uk>
2023-03-10 13:41:54 +0000

    Expand etopo to cover Aceh

--- R/CreateEtopo.R
+++ R/CreateEtopo.R
@@ -4,11 +4,15 @@
         require(raster)
         require(tidyverse)
 
+         rasterOptions(tolerance = 10)
+
         # Load ETOPO data
             etopo1 <- raster::raster(file.path(path, "Data", "exportImage.tiff")) %>%
                     raster::aggregate(fact=3)
             etopo2 <- raster::raster(file.path(path, "Data", "exportImage(1).tiff")) %>%
                     raster::aggregate(fact=3)
-            etopo<-raster::merge(etopo1, etopo2)
+            etopo3 <- raster::raster(file.path(path, "Data", "exportImage(2).tiff")) %>%
+                    raster::aggregate(fact=3)
+            etopo<-raster::merge(etopo1, etopo2, etopo3, tolerance=0.2)
 
         usethis::use_data(etopo, overwrite=TRUE)
